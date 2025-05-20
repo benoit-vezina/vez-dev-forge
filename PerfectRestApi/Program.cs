501434d841af10473b08e5c131e156419f2235a4
Benoit Vézina <benoit.vezina14@outlook.com>
2025-05-20 15:09:18 -0400

    Minimal logging setup

--- PerfectRestApi/Program.cs
+++ PerfectRestApi/Program.cs
@@ -7,12 +7,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers()
     .AddNewtonsoftJson(); // Add Newtonsoft JSON support for JsonPatch
 builder.Services.AddDbContext<MyContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
 builder.Services.AddScoped<ICookieService, CookieService>();
+builder.Logging.AddAzureWebAppDiagnostics();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 app.MapControllers();
+app.MapGet("/", () => "Hello, world!");
 
 app.Run();
